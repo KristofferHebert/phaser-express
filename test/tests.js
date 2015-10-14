cb3ecc3f43677128e07785c1de521e6397f3545f
= <kristoffer@kristofferhebert.com>
2015-10-13 19:31:42 -0700

    changed tests to es6

--- test/tests.js
+++ test/tests.js
@@ -1,33 +1,33 @@
 'use strict'
 import request from 'supertest'
 
-describe('loading express', function() {
+describe('loading express', ()=> {
 	let server
-	beforeEach(function() {
+	beforeEach(()=> {
 		delete require.cache[require.resolve('../server/server')]
 		server = require('../server/server')
 	})
-	afterEach(function(done) {
-		server.close(function () {
+	afterEach((done)=> {
+		server.close(()=> {
 			done()
 		})
 	})
-	it('responds to /', function testSlash(done) {
+	it('responds to /', (done)=> {
 		request(server)
 			.get('/')
 			.expect(200, done)
 	})
-	it('responds 500 to get scores', function testScores(done) {
+	it('responds 500 to get scores', (done)=> {
 		request(server)
 			.get('/scores')
 			.expect(500, done)
 	})
-	it('responds 500 to post scores', function testScores(done) {
+	it('responds 500 to post scores', (done)=> {
 		request(server)
 			.post('/scores')
 			.expect(500, done)
 	})
-	it('404 everything else', function testPath(done) {
+	it('404 everything else', (done)=> {
 		request(server)
 			.get('/foo/bar')
 			.expect(404, done)
